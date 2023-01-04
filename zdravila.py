2cd0fde2c2a54b832705d97a26315eeb01418cea
Gregor <gregorkuhelj3@gmail.com>
2023-01-04 18:24:22 +0100

    Dodan json file in pakiranje

--- zdravila.py
+++ zdravila.py
@@ -1,6 +1,8 @@
 from dobihtml import DobiHtml
-
+import time
 from bs4 import BeautifulSoup
+import json
+
 
 odziv = DobiHtml.dobi_html('http://www.cbz.si/zzzs/pao/bazazdr2.nsf/pregled13?openview&count=20000%27')
 soup = BeautifulSoup(odziv, 'html.parser')
@@ -9,29 +11,38 @@ vrstice_zdravila = soup.find_all('tr', valign = 'top')
 
 # pridobivamo podatke iz cbz.si
 
+zdravila = dict()
+
 for vrstica_zdravilo in vrstice_zdravila:
+    podatki_zdravila = dict()
+    
     td_ime = vrstica_zdravilo.find_all('td', class_ = 'textbarva0 gen-5')[0]
     td_sifra = vrstica_zdravilo.find_all('td', class_ = 'textbarva0 ts-s-4-6 ts-sez-c')[0]
     td_imetnik_dovoljenja = vrstica_zdravilo.find_all('td', class_ = "textbarva1")[1]
     td_cena = vrstica_zdravilo.find_all('td', class_ = 'textbarva4', width = '165')[0]
-    td_pakiranje = vrstica_zdravilo.find_all('td', class_ = 'textbarva02', width = '380')[0]
-
+    
+    
+    td_pakiranje = vrstica_zdravilo.find_all('td', class_ = 'textbarva02')[0]
+    
+    a_pakiranje = td_pakiranje.find_all('a', class_ = '', href = '#')[0]
+    pakiranje = a_pakiranje.contents[0]
+    
 
     a = td_ime.find_all('a')[1]
     ime = a.contents[0]
     sifra = td_sifra.contents[0]
     imetnik_dovoljenja = None if td_imetnik_dovoljenja.contents == [] else td_imetnik_dovoljenja.contents[0]
     cena = td_cena.find_all('center')[0].contents[0]
-    # pakiranje = td_pakiranje.find_all('title')[0].contents
-    # print(pakiranje)
+    
+    
+    podatki_zdravila['Šifra'] = sifra
+    podatki_zdravila['Imetnik dovoljenja'] = imetnik_dovoljenja
+    podatki_zdravila['Cena'] = cena
+    podatki_zdravila['Pakiranje'] = pakiranje
+    # print(ime,sifra, imetnik_dovoljenja, cena) 
 
-    print(ime,sifra, imetnik_dovoljenja, cena) 
+    zdravila[ime] = podatki_zdravila
 
+with open('zdravila.json', 'w', encoding='UTF-8') as file:
+    json.dump(zdravila,file)
 
-
-
-# for vrstica_zdravilo in vrstice_zdravila:
-#     td_sifra = vrstica_zdravilo.find_all('td', class_ = 'textbarva0 ts-s-4-6 ts-sez-c')[0] # ce ni indeksou vrne tabelo z 1 el, ti pa isces vrednost el
-#     #a = td_ime.find_all('a')[1]
-#     sifra = td_sifra.contents[0]
-#     print(sifra)
